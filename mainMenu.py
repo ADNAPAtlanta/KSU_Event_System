ee853d4a9cbfc33654b9e307586bb2037e53124a
Chris <Cfarley95@gmail.com>
2016-10-31 22:14:19 -0400

    v1

--- mainMenu.py
+++ mainMenu.py
@@ -1,4 +1,8 @@
 from firebase import firebase
+import smtplib
+from email.mime.image import MIMEImage
+from email.mime.multipart import MIMEMultipart
+from geopy.geocoders import Nominatim
 
 try:
     # for Python2
@@ -6,6 +10,7 @@ try:
 except ImportError:
     # for Python3
     from tkinter import *
+
 
 ksuBuildings = {"Baily Athletic Facility": "220 Kennesaw State Univ Rd NW Kennesaw, GA 30144",
                 "Bailey Performance Hall":"488 Prillaman Way NW Kennesaw, GA 30144",
@@ -111,8 +116,14 @@ ksuBuildingsOrdered = ["Baily Athletic Facility",
                 "University Village",
                 "KSU Place Apartments",
                 "Other"]
+days = ["01", "02", "03","04","05","06","07","08","09","10","11","12","13","14","15","16","17","18","19","20","21","22","23","24",
+        "25","26","27","28","29","30","31"]
 
-                
+months = ["Jan", "Feb", "Mar", "Apr",
+	   			    "May", "Jun", "July", "Aug",
+				    "Sep", "Oct", "Nov", "Dec"]
+
+years = ["2016","2017","2018"]
             
 
 
@@ -120,8 +131,8 @@ ksuBuildingsOrdered = ["Baily Athletic Facility",
 class eventForm:
     def __init__(self,master):
         self.master = master
-        master.title("KSU Event System")
-        master.minsize(width=340,height=300)
+        master.title("KSU Event System (organization ver.)")
+        master.minsize(width=300,height=300)
         self.type = StringVar()
         self.type.set("Vacation")
 
@@ -134,12 +145,22 @@ class eventForm:
         self.lat = DoubleVar()
         self.longitude = DoubleVar()
         self.date = StringVar()
+        self.category = StringVar()
         self.food = StringVar()
+        self.food.set("No")
         self.alcohol = StringVar()
+        self.alcohol.set("No")
         self.merchandise = StringVar()
+        self.merchandise.set("No")
 
         self.vacation = StringVar()
         self.date = StringVar()
+        self.day = StringVar()
+        self.day.set("01")
+        self.month = StringVar()
+        self.month.set("Jan")
+        self.year = StringVar()
+        self.year.set("2016")
 
         #Entry labels
         self.nameLabel = Label(master, text="Name of Event",underline=0)
@@ -153,18 +174,22 @@ class eventForm:
         self.organizationEntry.grid(row=2,column=2,padx=5)
         
         self.dateLabel = Label(master, text="Date",underline=0)
-        self.dateLabel.grid(row=1,column=3)
-        self.dateEntry = Entry(master,bd=3,textvariable=self.date)
-        self.dateEntry.grid(row=2,column=3,padx=5)
+        self.dateLabel.grid(row=1,column=4)
+        self.dayOption = OptionMenu(master,self.day,*days)
+        self.dayOption.grid(row=2,column=3)
+        self.monthOption = OptionMenu(master,self.month,*months)
+        self.monthOption.grid(row=2,column=4)
+        self.yearOption = OptionMenu(master,self.year,*years)
+        self.yearOption.grid(row=2,column=5)
         
         self.descriptionLabel = Label(master, text="Description",underline=0)
-        self.descriptionLabel.grid(row=3,column=2)
+        self.descriptionLabel.grid(row=3,column=1)
         self.descriptionEntry = Text(master,bd=3,height=5)
-        self.descriptionEntry.grid(row=4,column=1,columnspan=3)
+        self.descriptionEntry.grid(row=4,column=1,columnspan=2)
 
         self.buildingLabel = Label(master, text="Building",underline=0)
         self.buildingLabel.grid(row=5,column=1)
-        self.building.set("Select building.")
+        self.building.set("Other")
         self.buildingSelection = OptionMenu(master,self.building,*ksuBuildingsOrdered)
         self.buildingSelection.grid(column=1)
 
@@ -174,6 +199,7 @@ class eventForm:
         self.addressLabel.grid(row=5,column=2,sticky=W)
         self.addressEntry = Entry(master,textvariable=self.address,bd=3)
         self.addressEntry.grid(row=6,column=2,sticky=W)
+
 
         self.foodChoiceLabel = Label(master, text="Is food available?",underline=0)
         self.foodChoiceLabel.grid(column=1)
@@ -196,16 +222,35 @@ class eventForm:
         self.merchandiseChoiceNo = Radiobutton(master,text="No",variable=self.merchandise,value="No")
         self.merchandiseChoiceNo.grid(column=1)
 
+        self.submitButton = Button(master,text="Submit",command=self.submit)
+        self.submitButton.grid(column=2)
+
+    def submit(self):
+        name = self.name.get()
+        organization = self.organization.get()
+        date = self.date.get()
+        description = self.description.get()
+        if self.building.get() == "Other":
+            address = self.address.get()
+        else:
+            address = ksuBuildings[self.building.get()]
+
+        food = self.food.get()
+        alcohol = self.alcohol.get()
+        merchandise = self.merchandise.get()
+        print(name,organization,date,description,address,food,alcohol,merchandise)
+
+
+
+
+
+
+
 
 
 
         
 
-        
-
-        
-    
-        
 
 if __name__ == "__main__":
     root = Tk()
